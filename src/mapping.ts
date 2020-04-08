1ae15b25081fcbe509a7407cfe9bb50c6b57a9a9
Rohit Narurkar <rohitnarurkar@gmail.com>
2020-04-09 00:39:07 +0530

    added block parameter for query conditions

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,13 +3,14 @@ import { Contract, NewRsTx } from "../generated/Contract/Contract"
 import { Transaction } from "../generated/schema"
 
 export function handleNewRsTx(event: NewRsTx): void {
-  let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  let id = event.transaction.hash.toHex()
   let entity = Transaction.load(id)
 
   if (entity == null) {
     entity = new Transaction(id)
   }
 
+  entity.block = event.params.block
   entity.noncePoint = event.params.noncePoint
   entity.encryptedRecipient = event.params.encryptedRecipient
 
